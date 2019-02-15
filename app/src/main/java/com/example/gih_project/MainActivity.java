62f021a473c9db82aa9c6e5586a683334e01d3f8
3dc0deR <jellybean0079@gmail.com>
2019-02-15 20:18:37 +0530

    login functionality complete and working

--- app/src/main/java/com/example/gih_project/MainActivity.java
+++ app/src/main/java/com/example/gih_project/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.gih_project;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,4 +15,5 @@ public class MainActivity extends AppCompatActivity {
         //setContentView(R.layout.activity_main);
         startActivity(new Intent(this, LoginActivity.class));
     }
+
 }
